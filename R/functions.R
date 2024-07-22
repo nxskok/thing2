e27c5de3717d9aa90c16030d96189b6ba4e850cc
Ken Butler <ken.butler@utoronto.ca>
2024-07-22 15:16:33 -0400

    more functions

--- R/functions.R
+++ R/functions.R
@@ -4,12 +4,13 @@
 #' missings and translate numbered groups to birth types
 #'
 #' @param my_url Input url of wide data (optional)
-#' @returns a tidy (longer) version of the wide data with missings removed
+#' @return a tidy (longer) version of the wide data with missings removed
+#' @export
 #' @examples
 #' read_raw()
 #'
 read_raw <- function(my_url = "http://ritsokiguess.site/datafiles/births_wide.csv") {
-  births_wide <- read_csv(my_url)
+  births_wide <- read_csv(my_url, show_col_types = FALSE)
   births_wide %>%
     pivot_longer(everything(), names_to = "birth_type", values_to = "cortisol", values_drop_na = TRUE) -> births0
   # make lookup table of birth types
@@ -23,3 +24,34 @@ read_raw <- function(my_url = "http://ritsokiguess.site/datafiles/births_wide.cs
   births0 %>% left_join(type_names, join_by("birth_type" == "number")) %>%
     select(-birth_type)
 }
+
+#' Draw a boxplot
+#'
+#' Draw a boxplot of a dataframe with columns called `type` and `cortisol`
+#'
+#' @param x input dataframe with the required columns
+#' @return a boxplot from ggplot
+#' @export
+#'
+#'
+draw_boxplot <- function(x) {
+  ggplot(x, aes(x = type, y = cortisol)) + geom_boxplot()
+}
+
+
+#' Run Welch anova
+#'
+#' Run Welch ANOVA (not assuming equal variances) on a dataframe with columns called `type` and `cortisol`
+#'
+#' @param x input dataframe with required columns
+#' @return the output from Welch ANOVA plus Games-Howell test (in a `list`)
+#' @export
+#'
+run_welch <- function(x) {
+  ans1 <- oneway.test(cortisol ~ type, data = x)
+  ans2 <- gamesHowellTest(cortisol ~ factor(type), data = x)
+  list(ans1, ans2)
+}
+
+
+
